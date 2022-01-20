7c4ac20355b3bbb0eba374f045119a9d71fef05f
Delta Bravo <76425899+Dleta@users.noreply.github.com>
2022-01-20 16:53:34 +0100

    add bash

---
+++ dev.bash
@@ -0,0 +1,55 @@
+#!/bin/bash
+# homebrew extensions
+brew tap homebrew/services
+
+# development tools
+brew install git
+brew install php
+brew install mysql
+brew install phpmyadmin
+brew install sass/sass/sass
+brew install --cask github
+brew install --cask visual-studio-code
+brew install iterm2
+
+# browsers
+# brew install --cask google-chrome
+brew install --cask firefox
+
+# debugging
+# brew install --cask wireshark
+
+# ctf tools
+# brew install nmap
+# brew install gobuster
+
+# virtualization
+# brew install --cask virtualbox
+
+# java
+# brew install --cask scenebuilder
+# brew install --cask intellij-idea
+
+# c
+# brew install --cask clion
+
+# communication
+brew install discord
+# brew install --cask microsoft-outlook
+
+# support
+brew install --cask anydesk
+brew install --cask balenaetcher
+
+# maintenance
+# brew install --cask onyx
+
+# password manager (uncomment if needed)
+brew install --cask keepassxc
+
+# design (uncomment if needed)
+# brew install --cask adobe-creative-cloud
+# brew install --cask drawio
+
+# music
+# brew install --cask spotify
